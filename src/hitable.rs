4a96099b7b2b7d66f2d35327db2113f1804043d4
Martin Matusiak <numerodix@gmail.com>
2019-09-01 16:40:22 +1000

    simplify

--- src/hitable.rs
+++ src/hitable.rs
@@ -12,14 +12,6 @@ impl HitRecord {
     pub fn new(t: f32, p: Vec3, normal: Vec3) -> Self {
         HitRecord { t, p, normal }
     }
-
-    pub fn empty() -> Self {
-        HitRecord {
-            t: 0.0,
-            p: Vec3::new(0.0, 0.0, 0.0),
-            normal: Vec3::new(0.0, 0.0, 0.0),
-        }
-    }
 }
 
 
